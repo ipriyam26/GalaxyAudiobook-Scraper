6bc0fdab97ae30c4c4bde8010f12d641fe91e34d
ipriyam26 <ipriyam26@gmail.com>
2022-09-14 15:17:38 +0530

    found some files

--- galaxy/spiders/galaxy_book.py
+++ galaxy/spiders/galaxy_book.py
@@ -1,3 +1,4 @@
+
 import scrapy
 from scrapy.linkextractors import LinkExtractor
 from scrapy.spiders import CrawlSpider, Rule
@@ -9,15 +10,20 @@ class GalaxyBookSpider(CrawlSpider):
     start_urls = ['https://goldenaudiobooks.com/']
 
     rules = (
-        Rule(LinkExtractor(allow=r'/'), callback='parse_item', follow=True),
+        Rule(LinkExtractor(allow=r'/*'), follow=True, callback='parse_item'),
+        Rule(LinkExtractor(allow=r'category/'), follow=True),
+        Rule(LinkExtractor(allow=r'category/*'),  follow=True),
     )
 
     def parse_item(self, response):
-        # find the title
-        title = response.css('.entry-title').extract_first()
-        item = {
-            'title': title,
-            'url': response.url
-        }
-
-        yield item
+        title = response.css('.entry-title::text').extract_first()
+        image = response.css(".entry-content").re(
+            "https:\/\/goldenaudiobook.b-cdn.net\/wp-content\/uploads\/[0-9]{4}\/[0-9]{2}\/\S*\.jpg")
+        if image:
+            image = image[0]
+        audios = list(set(response.css(
+            ".entry-content").re("https:\/\/ipaudio.club\/wp-content\/uploads\/\S*mp3\?_=[0-9]+")))
+        author = response.css(".tags-links a").css("::text").get(default="Unknown")
+        tags = response.css("#main .single").css("::text").getall()
+        
+        yield {'title': title, 'url': response.url, 'image': image, 'audios': audios, 'author': author,'tags': tags }
